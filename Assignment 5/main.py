bd660ab99e149b5bf88a80231ee1fb9f4e1a003a
Hyperflip <44472080+Hyperflip@users.noreply.github.com>
2021-09-23 18:39:06 +0200

    implemented p-value calculation

--- Assignment 5/main.py
+++ Assignment 5/main.py
@@ -2,6 +2,7 @@ from sklearn.datasets import make_regression
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from sklearn.feature_selection import f_regression
 
 if __name__ == '__main__':
     X, y = make_regression(n_samples=100, n_features=1, noise=25)
@@ -22,7 +23,7 @@ if __name__ == '__main__':
     plt.hist(X, bins=10)
     plt.show()
 
-    # calculate MSE and R2
+    # calculate MSE, R2 and p-value
     n = len(X)
     predictions = reg.predict(X)
     # residual sum of squares
@@ -30,6 +31,9 @@ if __name__ == '__main__':
     tss = np.sum((np.mean(y) - y) ** 2)
     mse = rss / n
     r_2 = 1 - (rss / tss)
+    # p-value
+    _, p_value = f_regression(X, y)
 
     print('MSE:', mse)
     print('R2:', r_2)
+    print('p-value:', p_value[0])
